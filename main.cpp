2e968be38f3bb8091426cecd6992ae883e4c7cae
pspkzar <cesar.perdigao92@gmail.com>
2014-03-28 22:42:59 +0000

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <optix_world.h>
+#include <optixu/optixpp_namespace.h>
+
+int main(int argc, char **argv){
+	optix::Context c = optix::Context::create();
+	return 0;
+}
---
+++ path.cu
@@ -0,0 +1,2 @@
+#include <optix_world.h>
+
