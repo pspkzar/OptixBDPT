449d539589221ef507923e5e38b2bc92ada66c43
pspkzar <cesar.perdigao92@gmail.com>
2014-04-06 19:38:29 +0100

    Prototype path tracer

--- src/path.cu
+++ src/path.cu
@@ -1,4 +1,8 @@
 #include <optix_world.h>
+#include "commonStructs.h"
+#include "random.h"
+
+using namespace optix;
 
 struct PathResult{
 	float4 result;
@@ -11,7 +15,95 @@ struct PathResult{
 	bool finished;
 };
 
+struct ShadowResult{
+	bool in_shadow;
+};
+
 rtDeclareVariable(PathResult, current_path_result, rtPayload, );
+rtDeclareVariable(ShadowResult, current_shadow_result, rtPayload, );
+
+rtDeclareVariable(uint2, launch_index, rtLaunchIndex, );
+rtDeclareVariable(uint2, launch_dim,   rtLaunchDim, );
+
+//camera properties
+rtDeclareVariable(float3, eye, , );
+rtDeclareVariable(float3, look, , );
+rtDeclareVariable(float3, up, , );
+rtDeclareVariable(float3, right, , );
+//lens (for depth of field
+rtDeclareVariable(float, lens_radius, , );
+rtDeclareVariable(float, focal_dist, , );
+
+//frame number to make sure result is different every frame
+rtDeclareVariable(int, frame, , );
+//samples for stratified sampling
+rtDeclareVariable(int, sqrt_num_samples, , )=2;
+
+//output buffer
+rtBuffer<float4, 2> output;
+
+//top object to start tracing rays
+rtDeclareVariable(rtObject, top_object, , );
+
+RT_PROGRAM void camera(){
+	unsigned int seed = tea<16>(launch_dim.x*launch_index.y+launch_index.x, frame);
+
+	float2 inv_screen=1.f/(make_float2(launch_dim)) *2.f;
+	float2 pixel = (make_float2(launch_index)) * inv_screen - 1.f;
+	float2 jitter_scale = inv_screen / sqrt_num_samples;
+
+	int samples_per_pixel = sqrt_num_samples * sqrt_num_samples;
+
+	float4 result = make_float4(0.0f);
+
+	do{
+		int x = samples_per_pixel%sqrt_num_samples;
+		int y = samples_per_pixel/sqrt_num_samples;
+		float2 jitter = make_float2(x-rnd(seed), y-rnd(seed));
+		float2 d = pixel + jitter*jitter_scale;
+		float3 ray_origin = eye;
+		float3 ray_direction = normalize(d.x * right + d.y * up + look);
+
+		PathResult ray_result;
+		ray_result.atenuation=make_float4(1.f);
+		ray_result.count_emissive=true;
+		ray_result.depth=0;
+		ray_result.result=make_float4(0.f);
+		ray_result.seed=seed;
+		ray_result.finished=false;
+
+		for(;;){
+
+			Ray ray = optix::make_Ray(ray_origin, ray_direction, PathRay, 0.001, RT_DEFAULT_MAX);
+			rtTrace(top_object, ray, ray_result);
+
+			if(ray_result.finished)
+				break;
+
+			ray_origin=ray_result.position;
+			ray_direction=ray_result.direction;
+
+		}
+
+		result+=ray_result.result;
+		seed=ray_result.seed;
+
+	}while(--samples_per_pixel);
+
+	result/=sqrt_num_samples * sqrt_num_samples;
+
+	if(frame>1){
+		float a = 1.f/float(frame);
+		float b = float(frame-1)*a;
+		float4 old_color=output[launch_index];
+		output[launch_index]=a*result+b*old_color;
+	}
+	else{
+		output[launch_index]=result;
+	}
+
+}
+
 
 __device__ __inline__ void calc_direct_light(){
 
@@ -28,8 +120,6 @@ RT_PROGRAM void closest_hit(){
 	float pdiff=(Kd.x+Kd.y+Kd.z)*0.33333333333333333333333333333f;
 	float pspec=(Ks.x+Ks.y+Ks.z)*0.33333333333333333333333333333f;
 	pspec*=fminf(1.f, optix::dot(current_ray.direction, shading_normal)*(Ns+2.f)/(Ns+1.f));
-
-	int * a = (int*) malloc(sizeof(int));
 
 	//randomly select the type of contribution
 	float r=rnd(current_path_result.seed);
